d7a8df107ebbc36e101b069b4dbbf10d5a575d6b
Saloni <salonimehta2127@gmail.com>
2021-05-02 14:35:45 -0400

    commit tree 519a6476d0372b892e018f709894706de9162612

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride<=400)
+  {
+    return "This one is on me!";
+  }
+  else if(ride>2000 && ride<2500)
+  {
+    return "I will gladly take your thirty bucks.";
+  }
+  else
+  {
+    return "No can do.";
+  }
+}
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city){
+const message= city==='NYC'? "Ok, sounds good." : "No go.";
+return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+ternaryCheckCity('Pittsburgh');
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip)
+{
+  case 'generous':
+    response="Thank you so much.";
+    break;
+  case 'not as generous':
+    response="Thank you.";
+    break;
+  case 'thanks for everything':
+    response='Bye.';
+    break;
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+return response;
 }
